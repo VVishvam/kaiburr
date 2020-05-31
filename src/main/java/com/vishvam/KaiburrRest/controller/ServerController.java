155801ec1f432e37d49a1b0064a6169e141ebd41
V2vv <vishvam.vyas@getfitso.com>
2020-06-01 03:54:13 +0530

    Asd

--- src/main/java/com/vishvam/KaiburrRest/controller/ServerController.java
+++ src/main/java/com/vishvam/KaiburrRest/controller/ServerController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins="*",allowHeader="*")
 public class ServerController {
 
     private final ServerService serverService;
